1403aeb23b3f9639ac9671f14de8363634c4b850
　testlp <textlp@163.com>
2018-07-31 08:44:28 +0800

    no commit message

---
+++ app/src/main/java/com/sunwuyou/swymcx/ui/field/FieldLocalRecordActivity.java
@@ -0,0 +1,86 @@
+package com.sunwuyou.swymcx.ui.field;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.sunwuyou.swymcx.R;
+import com.sunwuyou.swymcx.app.BaseHeadActivity;
+import com.sunwuyou.swymcx.dao.FieldSaleDAO;
+
+/**
+ * Created by liupiao on
+ * 2018/7/30.
+ * content
+ */
+public class FieldLocalRecordActivity extends BaseHeadActivity implements View.OnTouchListener {
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            if (menuPopup == null || !menuPopup.isShowing()) {
+//                startActivity(new Intent(FieldLocalRecordActivity.this, FieldEditActivity.class).putExtra("fieldsaleid", tasks.get(position).getId()));
+//            } else {
+//                menuPopup.dismiss();
+//                WindowManager.LayoutParams v1 = getWindow().getAttributes();
+//                v1.alpha = 1f;
+//                getWindow().setAttributes(v1);
+//            }
+        }
+    };
+    private View root;
+    private ListView listView;
+    private FieldLocalRecordAdapter adapter;
+    private ProgressDialog progressDialog;
+    private FieldSaleDAO fieldSaleDAO;
+
+    @Override
+    public int getLayoutID() {
+        return R.layout.act_field_local_records;
+    }
+
+    @Override
+    public void initView() {
+        this.root = this.findViewById(R.id.root);
+        this.root.setOnTouchListener(this);
+        listView = this.findViewById(R.id.listView);
+        listView.setOnTouchListener(this);
+        adapter = new FieldLocalRecordAdapter(this);
+        this.listView.setAdapter(this.adapter);
+        this.listView.setOnItemClickListener(itemClickListener);
+        this.listView.setChoiceMode(3);
+//        this.listView.setMultiChoiceModeListener(this.muliChoiceModeLisener);
+//        this.refresh();
+        progressDialog = new ProgressDialog(this);
+        this.progressDialog.setProgressStyle(1);
+        this.progressDialog.setMessage("正在上传中...");
+        this.progressDialog.setCancelable(false);
+        fieldSaleDAO = new FieldSaleDAO();
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setActionBarText() {
+        setTitle("我的销售");
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+//        if (menuPopup != null && (FmenuPopup.isShowing())) {
+//            menuPopup.dismiss();
+//            WindowManager.LayoutParams v0 = getWindow().getAttributes();
+//            v0.alpha = 1f;
+//            getWindow().setAttributes(v0);
+//        }
+        return false;
+    }
+}
